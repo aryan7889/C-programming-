2fcc9746ec596617d6e3cc3f45ba6c89fb2dd505
aryan7889 <aryan.goyalsons28@gmail.com>
2025-07-02 12:52:49 +0530

    C

---
+++ variables.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+int main(){
+    int age = 25;
+    printf("my age is %d much years old\n",age);
+
+    // anothe rgood exmaple of the int could be the quantity
+    int quantity = 2;
+    printf("hey i have ordered %d x items from the store.",quantity);
+    return 0;
+}
